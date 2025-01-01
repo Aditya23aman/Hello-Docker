3f06121e2f3982bbd772f50e6583f4b83404fccb
Aditya Aman <aditya23aman@gmail.com>
2025-01-01 21:38:14 +0530

    Please

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def home():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, World! </p>"
 
 if __name__ == "__main__":
     app.run(debug=True,host="0.0.0.0",port=5000)
